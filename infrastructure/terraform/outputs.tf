041c2d04b068579358ebc45cfc9b1a5ea1e571c9
durosyan <>
2025-07-21 15:59:02 +0000

    ++ starting on lambda deployment

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,3 +1,7 @@
+output "api_gateway_url" {
+  description = "API Gateway endpoint URL"
+  value       = aws_apigatewayv2_api.http_api.api_endpoint
+}
 output "website_url" {
   description = "URL of the static website"
   value       = aws_s3_bucket.static_site.website_endpoint
